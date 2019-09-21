c0a3946adbe01c62b2118d8301104d8b2bdab948
Nathan Pillai <natepill@gmail.com>
2019-09-21 12:46:52 -0700

    [edit] refactored speech recognition for longer audio files via asynchronous request

--- google_speech_test.py
+++ google_speech_test.py
@@ -1,29 +1,42 @@
-import io
-import os
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import enums
 
-# Imports the Google Cloud client library
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
 
-# Instantiates a client
-client = speech.SpeechClient()
+def sample_long_running_recognize(storage_uri):
+    """
+    Transcribe long audio file from Cloud Storage using asynchronous speech
+    recognition
 
-# The name of the audio file to transcribe
-file_name = os.path.join(os.path.dirname(__file__),'example_audio.wav')
+    Args:
+      storage_uri URI for audio file in Cloud Storage, e.g. gs://[BUCKET]/[FILE]
+    """
 
-# Loads the audio into memory
-with io.open(file_name, 'rb') as audio_file:
-    content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
+    client = speech_v1.SpeechClient()
 
-config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code='en-US')
+    # storage_uri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw'
 
-# Detects speech in the audio file
-response = client.recognize(config, audio)
+    # Sample rate in Hertz of the audio data sent
+    sample_rate_hertz = 16000
 
-for result in response.results:
-    print('Transcript: {}'.format(result.alternatives[0].transcript))
+    # The language of the supplied audio
+    language_code = "en-US"
+
+    # Encoding of audio data sent. This sample sets this explicitly.
+    # This field is optional for FLAC and WAV audio formats.
+    encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+    config = {
+        "sample_rate_hertz": sample_rate_hertz,
+        "language_code": language_code,
+        "encoding": encoding,
+    }
+    audio = {"uri": storage_uri}
+
+    operation = client.long_running_recognize(config, audio)
+
+    print(u"Waiting for operation to complete...")
+    response = operation.result()
+
+    for result in response.results:
+        # First alternative is the most probable result
+        alternative = result.alternatives[0]
+        print(u"Transcript: {}".format(alternative.transcript))
